eab1ec88bbfdfd36d0f991c869bc5d47dcb9c492
Pierre Chatard <chatard.pierre@gmail.com>
2023-04-19 16:31:54 +0200

    Add getDaysUntilBirthday function

---
+++ src/utils/getDaysUntilBirthday.test.ts
@@ -0,0 +1,64 @@
+import { describe, test, expect } from "vitest";
+import { getDaysUntilBirthday } from "./getDaysUntilBirthday";
+
+const aprilNineteenth = new Date(2023, 3, 19, 12);
+const exampleBirthDate = new Date(1997, 4, 1, 12);
+const earlyYearBirthDate = new Date(1997, 0, 1, 12);
+const lateYearBirthDate = new Date(1997, 11, 31, 12);
+const futureBirthDate = new Date(2033, 6, 1, 12);
+
+describe("getDaysUntilBirthday", () => {
+  test("Returns something", () => {
+    expect(getDaysUntilBirthday(aprilNineteenth)).toBeDefined();
+  });
+
+  test("Returns a number", () => {
+    expect(getDaysUntilBirthday(aprilNineteenth)).toBeTypeOf("number");
+  });
+
+  test("Returns a number greater or equal than -1 and less than 366", () => {
+    expect(getDaysUntilBirthday(aprilNineteenth)).toBeLessThan(366);
+    expect(getDaysUntilBirthday(aprilNineteenth)).toBeGreaterThanOrEqual(-1);
+  });
+
+  test("Returns 0 if given birth date is today's date", () => {
+    expect(getDaysUntilBirthday(aprilNineteenth)).toEqual(0);
+  });
+
+  test("Returns 1 if given birth date is tomorrow's date", () => {
+    const tomorrow = new Date(aprilNineteenth);
+    tomorrow.setFullYear(1986);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    expect(getDaysUntilBirthday(tomorrow)).toEqual(1);
+  });
+
+  test("Returns 10 if given birth date is 10 days after today", () => {
+    const tenDaysLater = new Date(aprilNineteenth);
+    tenDaysLater.setFullYear(1986);
+    tenDaysLater.setDate(tenDaysLater.getDate() + 10);
+    expect(getDaysUntilBirthday(tenDaysLater)).toEqual(10);
+  });
+
+  test("Returns 30 if given birth date is 30 days after today", () => {
+    const thirtyDaysLater = new Date(aprilNineteenth);
+    thirtyDaysLater.setFullYear(1986);
+    thirtyDaysLater.setDate(thirtyDaysLater.getDate() + 30);
+    expect(getDaysUntilBirthday(thirtyDaysLater)).toEqual(30);
+  });
+
+  test("Returns 12 when birthday is 01/05 and today is 19/04", () => {
+    expect(getDaysUntilBirthday(exampleBirthDate)).toEqual(12);
+  });
+
+  test("Returns 256 when birthday is 31/12 and today is 19/04", () => {
+    expect(getDaysUntilBirthday(lateYearBirthDate)).toEqual(256);
+  });
+
+  test("Returns 257 when birthday is 01/01 and today is 19/04", () => {
+    expect(getDaysUntilBirthday(earlyYearBirthDate)).toEqual(257);
+  });
+
+  test("Returns -1 if given birth date is in future", () => {
+    expect(getDaysUntilBirthday(futureBirthDate)).toEqual(-1);
+  });
+});
---
+++ src/utils/getDaysUntilBirthday.ts
@@ -0,0 +1,32 @@
+/**
+ * Returns the number of days until your birthday
+ * @param birthDate Date of birth
+ * @returns The number of days until your next birthday or -1 if you are not born yet.
+ */
+export function getDaysUntilBirthday(birthDate: Date) {
+  const today = new Date();
+  today.setHours(12);
+  today.setMinutes(0);
+  today.setSeconds(0);
+  today.setMilliseconds(0);
+  const todayInMilliseconds = today.getTime();
+
+  if (birthDate.getTime() > today.getTime()) {
+    return -1;
+  }
+
+  const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
+
+  // Get next birthday's date (we set it to this year or this year + 1 if the date has already passed this year)
+  const nextBirthday = new Date(birthDate);
+  nextBirthday.setFullYear(today.getFullYear());
+  if (today.getTime() > nextBirthday.getTime()) {
+    nextBirthday.setFullYear(nextBirthday.getFullYear() + 1);
+  }
+
+  const nextBirthdayInMilliseconds = nextBirthday.getTime();
+
+  return Math.floor(
+    (nextBirthdayInMilliseconds - todayInMilliseconds) / oneDayInMilliseconds
+  );
+}
